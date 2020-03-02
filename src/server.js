66c27c265123dd631976d201ba1ee257f0be6a33
Marcelo Martins <marcelomartinsdev@gmail.com>
2020-03-02 10:31:35 -0300

    mongodb server added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
+
 const app = express();
+
+mongoose.connect('mongodb+srv://projetox:admin@projetox-dbbhb.mongodb.net/dataproject?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);    

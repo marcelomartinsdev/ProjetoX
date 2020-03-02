70c0b4877a4a65e7e13e0ab091701808bab7b7cb
Marcelo Martins <marcelomartinsdev@gmail.com>
2020-03-02 11:22:44 -0300

    spot.js & spotcontroller

---
+++ src/models/Spot.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const SpotSchema = new mongoose.Schema({
+    thumbnail: String,
+    company: String,
+    prince: Number,
+    techs: [String], //vetor com varias strings dentro
+    user: {
+        type: mongoose.Schema.Types.ObjectId,    //gravar o Id do User
+        ref: 'User'                            
+    }
+});
+
+module.exports = mongoose.model('Spot', SpotSchema);

7cfb0a0eaf2765919d7ef4e211e9d7ec42a9b063
Marcelo Martins <marcelomartinsdev@gmail.com>
2020-03-02 10:03:16 -0300

    routes.js added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);    
 
-app.post('/users', (req, res) => {
-    return res.json(req.body);
-});
 
 app.listen(1234);

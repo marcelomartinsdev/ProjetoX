b1185774b1bfef6d21b0247920d3477d05121622
Marcelo Martins <marcelomartinsdev@gmail.com>
2020-03-02 09:54:27 -0300

    json test working

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/users', (req, res) => {
-    return res.json({ idade: req.body });
+    return res.json(req.body);
 });
 
 app.listen(1234);
